1b5afb12d0364d3856bf40376e0f8bd8178be713
TimurVerba <135266210+TimurVerba@users.noreply.github.com>
2023-09-10 15:11:22 +0300

    new colors

--- currency_checker.py
+++ currency_checker.py
@@ -1,7 +1,36 @@
 import requests
 import json
+from tabulate import tabulate
+from styles import stylize
 
-def get_exchange_rate(currency_code):
+currency_translations = {
+    'USD': 'US Dollar',
+    'EUR': 'Euro',
+    'PLN': 'Polish Zloty',
+    'BYN': 'Belarusian Ruble',
+    'CNY': 'Chinese Yuan',
+    'CZK': 'Czech Koruna',
+    'GBP': 'British Pound Sterling',
+}
+
+def get_exchange_data(currency_code):
+    """
+    Retrieves the exchange rate and currency name for a specified currency code
+    from the National Bank of Ukraine (NBU) API.
+
+    Parameters:
+        currency_code (str): The three-letter currency code (e.g., 'USD', 'EUR')
+        for the currency you want to get the exchange rate for.
+
+    Returns:
+        tuple or None: A tuple containing the currency name, currency code, and exchange rate as a float.
+        If the currency is not found or an error occurs during the request, it returns None.
+
+    Note:
+        - The function makes an HTTP request to the NBU API to retrieve the exchange rate data.
+        - The exchange rate is based on the Ukrainian hryvnia (UAH) as the base currency.
+        - The NBU API provides exchange rate data for various foreign currencies.
+    """
     # URL of the JSON file
     url = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json'
 
@@ -17,7 +46,7 @@ def get_exchange_rate(currency_code):
             # Find the exchange rate of the hryvnia to the specified currency
             for item in data:
                 if item['cc'] == currency_code:
-                    return item['rate']
+                    return item['rate'], currency_translations.get(currency_code)
 
             # If currency not found, return None
             return None
@@ -30,13 +59,21 @@ def get_exchange_rate(currency_code):
         print(f"An error has occurred: {e}")
         return None
 
+
 if __name__ == "__main__":
-    print("Here currency in NBU rate")
+    print("Here`s NBU hryvnia exchange rate to foreign currencies")
     currencies = ['USD', 'EUR', 'PLN', 'BYN', 'CNY', 'CZK', 'GBP']
+    headers = [stylize(header, "yellow", "bold")for header in ["Currency name", "Currency", "Rate"]]
+
+    exchange_rates = []
 
     for currency in currencies:
-        rate = get_exchange_rate(currency)
+        rate, currency_name = get_exchange_data(currency)
         if rate is not None:
-            print(f"Ukrainian hryvnia to {currency}: {rate}")
-        else:
-            print(f"Course {currency} not found or an error occurred.")
+            exchange_rates.append([stylize(currency_name, "", "bold"), stylize(currency, "green", "bold"),
+                                   stylize(rate, "red", "bold")])
+
+    colalign = ("center", "center", "center")
+
+    table = tabulate(exchange_rates, headers, tablefmt="grid", colalign=colalign)
+    print(table)
