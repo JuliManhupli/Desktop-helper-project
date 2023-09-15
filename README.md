f12d9c8caf5c13af26d8d505ed6cd02c885886e3
JuliManhupli <juliamangup@gmail.com>
2023-09-15 20:55:05 +0300

    edit readme

--- README.md
+++ README.md
@@ -34,6 +34,8 @@ You can install YourHelper from TestPyPI using pip:
 pip install -i https://test.pypi.org/simple/ yourhelper
 ```
 
+> **_Issues:_** There may be problems with automatically install dependencies that are not available on TestPyPi. You will need to install them manually.
+
 ## Usage
 
 To use YourHelper, follow these steps:
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_namespace_packages
 
 setup(name='yourhelper',
-      version='0.0.2',
+      version='0.0.4',
       description='YourHelper is a Python application that provides various tools and utilities in one place.',
       long_description=open("README.md", "r", encoding="utf-8").read(),
       long_description_content_type="text/markdown",
